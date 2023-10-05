0a3514f47af59e27abff076f3faeb2186be11282
Dan Coates <dcoates72@gmail.com>
2023-10-05 09:57:40 -0500

    Replace header

---
+++ nextwave_plugin.hpp
@@ -0,0 +1,8 @@
+namespace NextWave {
+
+  void init(void);
+  void do_process(const char* which_buffer);
+  void set_params(const char* settings_as_json);
+  void get_info(const char* which_info);
+}
+
