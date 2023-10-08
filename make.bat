f7874856dd02c970c093c312ba294380e73159a1
Dan Coates <dcoates72@gmail.com>
2023-10-08 14:42:18 -0500

    Runnable from git bash

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
 rem Run me from the build directory of the base source tree
+rem cd build; cmake ..   // need to do this once
 cmake --build build
 copy build\plugin_ximea\debug\plugin_ximea.dll build\debug
